133ffcf9bc1e46e455dec47283151b4e52b9ab70
my-kit <pavel979@gmail.com>
2017-05-26 12:16:12 +0300

    *Add

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>изучаем github</title>
+</head>
+<body>
+
+<section id="screen-1">
+	<div class="uk-container">
+		<h1>We are starting</h1>
+	</div>
+</section>
+	
+</body>
+</html>
